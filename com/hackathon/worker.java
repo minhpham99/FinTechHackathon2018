adc57cabdfc8aefcae4e77030987bd0694f594ca
Minh Pham <sc17mp@leeds.ac.uk>
2018-10-27 14:23:37 +0100

    Change directory and add BankAccount

--- com/hackathon/worker.java
+++ com/hackathon/worker.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.hackathon;
 import java.util.*;
 import java.io.*;
 
@@ -7,16 +7,18 @@ public class worker {
     private int age;
     private float wage;
     private int hours = 0;
+    private String bankAccount;
 
     private boolean ageVerified;
     private boolean payVerified;
     private boolean paidOnTime;
 
 
-    public worker (String name, int age, float wage) {
+    public worker (String name, int age, float wage, String bankAccount) {
         this.name = name;
         this.age = age;
         this.wage = wage;
+        this.bankAccount = bankAccount;
 
         this.ageVerified = false;
         this.payVerified = false;
@@ -32,6 +34,8 @@ public class worker {
     public String getName() {
         return name;
     }
+
+    public String getBankAccount() { return bankAccount; }
 
     public void setMonthlyHours(int newHours) {
         hours = newHours;
