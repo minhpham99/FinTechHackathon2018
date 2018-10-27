88c5d35217656b96dcfef70a1f0bfb1a3a955622
Minh Pham <sc17mp@leeds.ac.uk>
2018-10-27 19:56:45 +0100

    Add methods for parent company

--- com/hackathon/parentCompany.java
+++ com/hackathon/parentCompany.java
@@ -4,9 +4,22 @@ import java.util.HashMap;
 
 public class parentCompany {
     private String companyName;
-    private HashMap<String, supplier> supplierList = new HashMap<>();
+    private HashMap<String, supplier> supplierList;
     private supplier s;
 
-    public parentCompany(String name) { this.companyName = name; }
+    public parentCompany(String name) {
+        this.companyName = name;
+        supplierList = new HashMap<>();
+    }
     public String getName() { return companyName; }
+
+    public void addSupplier(String name, String location) {
+        supplier s = new supplier(name, location);
+        supplierList.put(name, s);
+    }
+
+    public void removeSupplier(String name) {
+        if(supplierList.containsKey(name)) supplierList.remove(name);
+        else System.out.println("Supplier does not exist in the list");
+    }
 }
