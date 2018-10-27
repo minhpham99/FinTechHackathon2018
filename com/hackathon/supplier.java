a377cc849d363f79fd2f822b5b583be2921f407a
Minh Pham <sc17mp@leeds.ac.uk>
2018-10-27 14:49:48 +0100

    Add Hashmap of workers

--- com/hackathon/supplier.java
+++ com/hackathon/supplier.java
@@ -1,4 +1,25 @@
 package com.hackathon;
 
+import javax.naming.Name;
+import java.util.*;
+
 public class supplier {
+
+    protected worker w;
+    protected HashMap<String, String> workerList = new HashMap<>();
+    protected int supplierID;
+    protected String supplierName;
+    protected String supplierLocation;
+
+
+    public void addWorker(String name, int age, float wages, String bankAccount, HashMap workerList) {
+        name = w.getName();
+        age = w.getAge();
+        wages = w.getExpectedPay();
+        bankAccount = w.getBankAccount();
+        worker newWorker = new worker(name, age, wages, bankAccount);
+        this.workerList.put(bankAccount, name);
+    }
+
+
 }
