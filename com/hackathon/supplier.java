54db858418a8fb512498de2ead5fd472ea347074
John Barbone <jbarbone@toolbox.local>
2018-10-27 15:57:13 +0100

    added constructor, changed stuff in variables, changed addWorker

--- com/hackathon/supplier.java
+++ com/hackathon/supplier.java
@@ -3,12 +3,18 @@ package com.hackathon;
 import java.util.*;
 
 public class supplier {
+                                                    // Removed worker from the class variables because the class doesn't want to be storing one single instance of a worker
+    protected HashMap<String, worker> workerList;
+//    protected int supplierID;                     // I don't think we need to have suppliers store their own ID, since they don't really care about their own ID
+    private String supplierName;
+    private String supplierLocation;
 
-    protected worker w;
-    protected HashMap<String, worker> workerList = new HashMap<>();
-    protected int supplierID;
-    protected String supplierName;
-    protected String supplierLocation;
+
+    public supplier (String name, String location) {
+        this.supplierName = name;
+        this.supplierLocation = location;
+        workerList = new HashMap<>();
+    }
 
     public int getID() { return supplierID; }
 
@@ -16,17 +22,16 @@ public class supplier {
 
     public String getLocation() { return supplierLocation; }
 
-    public void addWorker(String name, int age, float wages, String bankAccount, HashMap workerList) {
-        name = w.getName();
-        age = w.getAge();
-        wages = w.getExpectedPay();
-        bankAccount = w.getBankAccount();
-        worker newWorker = new worker(name, age, wages, bankAccount);
-        this.workerList.put(bankAccount, newWorker);
+
+    public void addWorker(String name, int age, float wages, String bankAccount) {
+        worker w = new worker(name, age, wage, bankAccount);
+        workerList.put(bankAccount, w);
     }
 
     public void removeWorker(String bankAccount) { workerList.remove(bankAccount); }
 
-    public boolean supplierVerified() {}
+    public boolean supplierVerified() {
+        return false;
+    }
 
 }
